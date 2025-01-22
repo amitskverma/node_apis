8218586a1bad6b11caca34d5490884e277af47e8
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-01-22 18:11:54 +0530

    Add changes for frontend react js connection

---
+++ cors.js
@@ -0,0 +1,5 @@
+const corsOptions = {
+    origin:'*'// Replace with your React app's URL
+  };
+  
+app.use(cors(corsOptions));
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express()
-const db = require('./db' )
-
+const express = require('express');
+const app = express();
+const db = require('./db'); // Assuming you use this module
+const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT
-console.log(process.env.DB_CONNECTION_STRING);
-console.log(port);
+app.use(cors());
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())  
 
-const personRoutes = require('./router/personRoutes')
+const personRoutes = require('./router/personRoutes');
+app.use('/', personRoutes); 
 
-
-app.use('/', personRoutes)
-app.listen(process.env.PORT || 3000, 
-() => console.log('Server is running on port 3000'));
+app.listen(process.env.PORT || 3000, () => { 
+  console.log(`Server is running on port ${process.env.PORT || 3000}`); 
+});
