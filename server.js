5bc35acd7723ff8c4a1bdbab92002cb189555828
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-08-26 15:47:14 +0530

    update for jenkins

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-// const express = require('express');
-// const app = express();
-// const db = require('./db'); // Assuming you use this module
-// const cors = require('cors');
-// require('dotenv').config();
-// app.use(cors());
-
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.json())  
-
-// const personRoutes = require('./router/personRoutes');
-// app.use('/', personRoutes); 
-
-// app.listen(process.env.PORT || 3000, () => { 
-//   console.log(`Server is running on port ${process.env.PORT || 3000}`); 
-// });
 const express = require('express');
 const app = express();
+const db = require('./db'); // Assuming you use this module
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, Node.js app is running!');
-});
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())  
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const personRoutes = require('./router/personRoutes');
+app.use('/', personRoutes); 
+
+app.listen(process.env.PORT || 3000, () => { 
+  console.log(`Server is running on port ${process.env.PORT || 3000}`); 
 });
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, Node.js app is running!');
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
