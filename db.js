a0d8f59c131ea89cf8f47e5051f83cb5fa0f35d9
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-09-07 16:58:57 +0530

    remove hardcode credential

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // define the mongoDB URL
-const mongoURL = "mongodb+srv://amitskverma:qytcEqz4xF7ZnQgn@cluster0.1pucky4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+//const mongoURL = "mongodb+srv://amitskverma:qytcEqz4xF7ZnQgn@cluster0.1pucky4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURL = process.env.DB_CONNECTION_STRING;
 // connect to the mongoDB database
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
